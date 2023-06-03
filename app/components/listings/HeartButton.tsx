957a0684fa10e8aa8f55f7757c26a63177571b5b
Ahmad Farhan <ahmadfarhanstwn@gmail.com>
2023-06-04 01:41:38 +0700

    favorite-functionality

--- app/components/listings/HeartButton.tsx
+++ app/components/listings/HeartButton.tsx
@@ -3,6 +3,7 @@
 import React from "react"
 import { SafeUser } from "../../types"
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import useFavorite from "../../hooks/UseFavorites"
 
 interface HeartButtonProps {
     listingId: string,
@@ -15,14 +16,15 @@ const HeartButton: React.FC<HeartButtonProps> = (
         currentUser
     }
 ) => {
-    const hasFavorite = false
-
-    const toggleFavorite = () => {}
+    const { hasFavorited, toggleFavorite} = useFavorite({
+        listingId,
+        currentUser
+    })
 
     return (
         <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
             <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
-            <AiFillHeart size={24} className={hasFavorite ? 'fill-rose-500' : 'fill-neutral-500/70'} />
+            <AiFillHeart size={24} className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'} />
         </div>
     )
 }
---
+++ app/hooks/UseFavorites.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+
+import { SafeUser } from "@/app/types";
+import useLoginState from "./UseLoginState";
+
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+
+  const loginState = useLoginState()
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favouriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(async (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+
+    if (!currentUser) {
+      return loginState.onOpen();
+    }
+
+    try {
+      let request;
+
+      if (hasFavorited) {
+        request = () => axios.delete(`/api/favorites/${listingId}`);
+      } else {
+        request = () => axios.post(`/api/favorites/${listingId}`);
+      }
+
+      await request();
+      router.refresh();
+      toast.success('Success');
+    } catch (error) {
+      toast.error('Something went wrong.');
+    }
+  }, 
+  [
+    currentUser, 
+    hasFavorited, 
+    listingId, 
+    loginState,
+    router
+  ]);
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  }
+}
+
+export default useFavorite;
